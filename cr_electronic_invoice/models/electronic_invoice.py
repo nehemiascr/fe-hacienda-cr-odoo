70f6d6ab2569e3ba3e18f029b41b3e84d97a947d
Nehemias Herrera <yo@nehemiascr.com>
2019-07-02 08:08:16 -0600

    handling 502 error from hacienda

--- cr_electronic_invoice/models/electronic_invoice.py
+++ cr_electronic_invoice/models/electronic_invoice.py
@@ -510,7 +510,7 @@ class ElectronicInvoice(models.TransientModel):
 			if 'ya fue recibido anteriormente' in object.respuesta_tributacion: object.state_tributacion = 'recibido'
 			if 'no ha sido recibido' in object.respuesta_tributacion: object.state_tributacion = 'pendiente'
 			return False
-		if response.status_code in (522, 524):
+		if response.status_code in (502,522, 524):
 			object.state_tributacion = 'error'
 			object.respuesta_tributacion = response.content
 			return False
