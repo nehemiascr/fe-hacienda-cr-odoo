58e0fa1b9ee2d19142c28ae201fa8066fa559f82
Nehemias Herrera <yo@nehemiascr.com>
2019-09-27 08:13:49 -0600

    [del] delete electronic_invoice modules

--- cr_electronic_invoice/models/product.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class AccountInvoice(models.Model):
-    _name = 'account.invoice'
-    _inherit = 'account.invoice'
-
--- cr_electronic_invoice/models/product_uom.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class ProductUom(models.Model):
-    _inherit = 'product.uom'
-
-    code = fields.Char('Código')
