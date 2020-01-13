b41b5e97e6b0d4928017d857de88fdc323f45517
Nehemias Herrera <yo@nehemiascr.com>
2020-01-12 20:18:41 -0600

    [fix] some adjustments

--- hr_expense_cr_electronic_invoice/models/account_invoice.py
+++ hr_expense_cr_electronic_invoice/models/account_invoice.py
@@ -10,29 +10,29 @@ class AccountInvoice(models.Model):
 
     _inherit = "account.invoice"
 
-
     @api.multi
     def action_convertir_en_gasto(self):
         _logger.info('%s of type %s' % (self, self.mapped('type')))
         for i, invoice in enumerate(self):
             if invoice.type == 'in_invoice' and invoice.state == 'draft':
                 _logger.info('%s/%s la podemos convertir %s' % (i, len(self), invoice))
-
                 product = self.env.ref('hr_expense.product_product_fixed_cost') or \
-                          self.env['product.product'].search(['can_be_expensed']).sorted()
+                          self.env['product.product'].search([('can_be_expensed', '=', True)]).sorted()[0]
                 quantity = 1.0
                 unit_amount = invoice.amount_total
-                tax_ids = invoice.tax_line_ids.mapped('tax_id').mapped('id')
+                tax_ids = invoice.tax_line_ids.mapped('tax_id')
                 if tax_ids:
-                    unit_amount -= (unit_amount * tax_ids[0] / 100)
+                    tax_id = tax_ids.sorted('amount')[-1]
+                    _logger.info('product %s unit_amount %s tax_id %s tax_id.amount %s' % (product, unit_amount, tax_id, tax_id.amount))
+                    unit_amount = (unit_amount * 100 / (100+tax_id.amount))
 
                 name = invoice.invoice_line_ids.sorted('price_subtotal')[-1].name[:32]
 
-                expense = self.env['hr.expense'].create({'name': '%s con %s' % (name, invoice.partner_id.name[:20]),
+                expense = self.env['hr.expense'].create({'name': '%s en %s' % (name, invoice.partner_id.name[:20]),
                                                'quantity': quantity,
                                                'product_id': product.id,
                                                'unit_amount': unit_amount,
-                                               'tax_ids': [(6, 0, tax_ids)],
+                                               'tax_ids': [(6, 0, [tax_id.id])] if tax_ids else None,
                                                'xml_supplier_approval': invoice.xml_supplier_approval,
                                                'fname_xml_supplier_approval': invoice.fname_xml_supplier_approval,
                                                'state_invoice_partner': '1',
@@ -48,9 +48,3 @@ class AccountInvoice(models.Model):
                         'view_mode': 'form',
                         'res_id': expense.id,
                         'target': 'self'}
-
-
-
-
-
-
