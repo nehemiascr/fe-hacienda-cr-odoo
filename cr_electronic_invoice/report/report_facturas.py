db71795fd1ffc48ed665d9d2f45f16da9b541951
Nehemias Herrera <yo@nehemiascr.com>
2021-02-26 00:46:59 +0000

    [UPD] Coloring Credit Notes rows in Excel Invoice Report

--- cr_electronic_invoice/report/report_facturas.py
+++ cr_electronic_invoice/report/report_facturas.py
@@ -2,6 +2,8 @@
 #     dev@fakturacion.com
 from odoo import api, models, fields, _
 import logging
+from datetime import datetime
+from dateutil import relativedelta
 
 _logger = logging.getLogger(__name__)
 
@@ -37,11 +39,20 @@ h = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K
 class FacturasReportWizard(models.TransientModel):
     _name = 'cr_electronic_invoice.facturas.report.wizard'
 
-    inicio = fields.Date(string="Fecha de inicio del reporte", required=True, default=fields.Date.today)
-    final = fields.Date(string="Fecha final del reporte", required=True, default=fields.Date.today)
+    def _default_inicio(self):
+        # primer día del mes pasado
+        return datetime.now().date().replace(day=1) - relativedelta.relativedelta(months=+1)
+
+    def _default_final(self):
+        # último día del mes pasado
+        return datetime.now().date().replace(day=1) + relativedelta.relativedelta(days=-1)
+
+
+    inicio = fields.Date(string="Fecha de inicio del reporte", required=True, default=_default_inicio)
+    final = fields.Date(string="Fecha final del reporte", required=True, default=_default_final)
     type = fields.Selection([   ('out_invoice', _(strings['out_invoice']['name'])),
                                 ('out_refund' , _(strings['out_refund']['name'])),
-                                ('in_invoice' , _(strings['in_invoice']['name']))   ])
+                                ('in_invoice' , _(strings['in_invoice']['name']))   ], default='out_invoice')
 
 
     @api.multi
@@ -79,112 +90,109 @@ class Facturas(models.AbstractModel):
                                                        ('date_invoice', '<=', data['form']['final']),
                                                        ('state', 'in', ('open','paid')),
                                                        ('company_id','=',self.env.user.company_id.id)
-                                                       ])
+                                                       ]).sorted(key=lambda f: (f.date_invoice, f.number))
         _logger.info('facturas %s' % facturas)
 
+        company_currency_id = facturas[0].company_id.currency_id if facturas else self.env.user.company_id.currency_id
         report_name = strings[data['form']['type']]['name']
+        color_nc = '#CCFF99'
         sheet = workbook.add_worksheet(report_name)
+        cell = workbook.add_format()
+        cell_nc = workbook.add_format({'bg_color': color_nc})
         bold = workbook.add_format({'bold': True})
-        money = workbook.add_format({'num_format': '₡#,##0.00'})
+        bold_nc = workbook.add_format({'bold': True, 'bg_color': color_nc})
+        money = workbook.add_format({'num_format': '₡ #,##0.00'})
+        money_nc = workbook.add_format({'num_format': '₡ #,##0.00', 'bg_color': color_nc})
         date = workbook.add_format({'num_format': 'dd/mm/yy'})
-        taxes = facturas.mapped('invoice_line_ids').mapped('invoice_line_tax_ids')
-        credit_notes = True if data['form']['type'] == 'out_invoice' and facturas.mapped('refund_invoice_ids') else False
-
+        date_nc = workbook.add_format({'num_format': 'dd/mm/yy', 'bg_color': color_nc})
+        tax_ids = facturas.mapped('invoice_line_ids').mapped('invoice_line_tax_ids')
+        row = 0 # Current Row index
         # Report Header
         # first row : Name     Vat     Date Range
-        sheet.write(0, 0, self.env.user.company_id.name, bold)
-        sheet.write(0, 2, self.env.user.company_id.vat, bold)
-        sheet.write(0, 6, 'Del %s al %s' % (data['form']['inicio'], data['form']['final']), bold)
+        sheet.write(row, 0, self.env.user.company_id.name, bold)
+        sheet.write(row, 2, self.env.user.company_id.vat, bold)
+        sheet.write(row, 6, 'Del %s al %s' % (data['form']['inicio'], data['form']['final']), bold)
         # second row : Title
-        sheet.write(1, 3, strings[data['form']['type']]['title'], bold)
+        row+=1 # 1
+        sheet.write(row, 3, strings[data['form']['type']]['title'], bold)
         # third row : Column Names
-        sheet.write(2, 0, strings[data['form']['type']]['vat'], bold)
-        sheet.write(2, 1, strings[data['form']['type']]['type'], bold)
-        sheet.write(2, 2, strings[data['form']['type']]['document'], bold)
-        sheet.write(2, 3, 'Consecutivo', bold)
-        sheet.write(2, 4, 'Fecha', bold)
-        sheet.write(2, 5, 'Subtotal', bold)
-        columna = 6
-        for i, tax in enumerate(taxes):
-            sheet.write(2, columna+i, tax.name, bold)
-        sheet.write(2, columna+len(taxes)+0, 'Total Impuestos', bold)
-        sheet.write(2, columna+len(taxes)+1, 'Total', bold)
-        sheet.write(2, columna+len(taxes)+2, 'Moneda', bold)
-        sheet.write(2, columna+len(taxes)+3, 'Tipo de Cambio', bold)
-        if credit_notes or data['form']['type'] == 'out_refund':
-            sheet.write(2, columna+len(taxes)+4, strings[data['form']['type']]['inverse'], bold)
+        row+=1 # 2
+        sheet.write(row, 0, strings[data['form']['type']]['vat'], bold)
+        sheet.write(row, 1, strings[data['form']['type']]['type'], bold)
+        sheet.write(row, 2, 'Documento', bold)
+        sheet.write(row, 3, 'Clave', bold)
+        sheet.write(row, 4, 'Consecutivo', bold)
+        sheet.write(row, 5, 'Fecha', bold)
+        sheet.write(row, 6, 'Subtotal', bold)
+        columna = 7
+        for i, tax in enumerate(tax_ids):
+            sheet.write(row, columna+i, tax.name, bold)
+        sheet.write(row, columna+len(tax_ids)+0, 'Total Impuestos', bold)
+        sheet.write(row, columna+len(tax_ids)+1, 'Total', bold)
+        sheet.write(row, columna+len(tax_ids)+2, 'Moneda', bold)
+        sheet.write(row, columna+len(tax_ids)+3, 'Tipo de Cambio', bold)
+        sheet.write(row, columna+len(tax_ids)+4, 'Total en la Moneda de la Factura', bold)
 
         # Report Lines
-        fila = 3
+        row+=1 # 3
         for i, factura in enumerate(facturas):
-            # Tipo de Cambio
-            exchange_rate = factura.amount_total_company_signed / factura.amount_total_signed if factura.amount_total_signed else 0
-            if factura.currency_id.name == 'USD' and exchange_rate == 1.0:
-                name = factura.date_invoice.strftime('%Y-%m-%d')
-                rate_id = self.env['res.currency.rate'].search([('name', '=', name)])
-                if rate_id:
-                    rate_id = rate_id[0]
-                    exchange_rate = rate_id['original_rate']
-            
-            # Contacto
-            sheet.write(i+fila, 0, factura.partner_id.vat or '')
-            # Nombre
-            sheet.write(i+fila, 1, factura.partner_id.name)
-            # Clave
-            sheet.write(i+fila, 2, factura.number_electronic)
-            # Consecutivo
-            sheet.write(i+fila, 3, factura.number_electronic[21:41] if factura.number_electronic else factura.number)
-            # Fecha
-            sheet.write(i+fila, 4, factura.date_invoice, date)
-            price_subtotal = sum(factura.invoice_line_ids.mapped('price_subtotal'))
-            if factura.refund_invoice_ids:
-                price_subtotal -= sum(factura.refund_invoice_ids.mapped('invoice_line_ids').mapped('price_subtotal'))
-            # Subtotal
-            sheet.write(i+fila, 5, round((price_subtotal if price_subtotal > 0 else 0) * exchange_rate, 2), money)
-            # sheet.write(indice+fila, 5, round(sum(factura.invoice_line_ids.mapped(lambda l: (l.price_unit * l.quantity) - l.price_subtotal)), 2))
-            # Impuestos
-            for j, tax in enumerate(taxes):
-                total = factura.tax_line_ids.filtered(lambda t: t.tax_id == tax).ensure_one().amount_total if tax in factura.tax_line_ids.mapped('tax_id') else 0.0
-                if factura.refund_invoice_ids:
-                    for refund_invoice in factura.refund_invoice_ids:
-                        total -= refund_invoice.tax_line_ids.filtered(lambda t: t.tax_id == tax).ensure_one().amount_total if tax in refund_invoice.tax_line_ids.mapped('tax_id') else 0.0
-                sheet.write(i+fila, columna+j, round((total if total > 0 else 0) * exchange_rate, 2), money)
-            # Total Impuestos
-            amount_total_tax = sum(factura.tax_line_ids.mapped('amount_total'))
-            if factura.refund_invoice_ids:
-                amount_total_tax -= sum(factura.refund_invoice_ids.mapped('tax_line_ids').mapped('amount_total'))
-            sheet.write(i+fila, columna+len(taxes)+0, round((amount_total_tax if amount_total_tax > 0 else 0) * exchange_rate, 2), money)
-            # Total
-            amount_total = factura.amount_total
-            if factura.refund_invoice_ids:
-                amount_total -= sum(factura.refund_invoice_ids.mapped('amount_total'))
-            
-            sheet.write(i+fila, columna+len(taxes)+1, round((amount_total if amount_total > 0 else 0) * exchange_rate, 2), money)
-            # Moneda
-            sheet.write(i+fila, columna+len(taxes)+2, factura.currency_id.name)
-            # Tipo de Cambio
-            sheet.write(i+fila, columna+len(taxes)+3, exchange_rate, money)
-            # Notas de Crédito
-            if credit_notes and factura.refund_invoice_ids:
-                credit_note_numbers = factura.refund_invoice_ids.mapped(lambda r: r.number_electronic[31:41])
-                credit_note_numbers = (', ').join(credit_note_numbers)
-                sheet.write(i+fila, columna+len(taxes)+4, credit_note_numbers)
-            elif data['form']['type'] == 'out_refund':
-                sheet.write(i+fila, columna+len(taxes)+4, factura.invoice_id.number_electronic[21:41])
+            invoice_ids = factura
+            invoice_ids += factura.mapped('refund_invoice_ids').filtered(lambda i: i.state in ('open', 'paid')).sorted(key=lambda f: f.number)
+            for j, i_id in enumerate(invoice_ids):
+                row += j
+                cell_format = cell_nc if i_id.type == 'out_refund' else cell
+                date_format = date_nc if i_id.type == 'out_refund' else date
+                mony_format = money_nc if i_id.type == 'out_refund' else money
+                # Tipo de Cambio
+                exchange_rate = i_id.amount_total_company_signed / i_id.amount_total_signed if i_id.amount_total_signed else 1.0
+                # Contacto
+                sheet.write(i+row, 0, i_id.partner_id.vat or '', cell_format)
+                # Nombre
+                sheet.write(i+row, 1, i_id.partner_id.name, cell_format)
+                # Documento
+                sheet.write(i+row, 2, strings[i_id.type]['document'], cell_format)
+                # Clave
+                sheet.write(i+row, 3, i_id.number_electronic, cell_format)
+                # Consecutivo
+                sheet.write(i+row, 4, i_id.number_electronic[21:41] if i_id.number_electronic else i_id.number, cell_format)
+                # Fecha
+                sheet.write(i+row, 5, i_id.date_invoice, date_format)
+                # Subtotal
+                price_subtotal = i_id.amount_untaxed_signed
+                sheet.write(i+row, 6, round(price_subtotal, 2), mony_format)
+                # Impuestos
+                for k, tax in enumerate(tax_ids):
+                    total = i_id.tax_line_ids.filtered(lambda t: t.tax_id == tax).ensure_one().amount_total * exchange_rate if tax in i_id.tax_line_ids.mapped('tax_id') else 0.0
+                    if i_id in factura.refund_invoice_ids:
+                        total *= -1
+                    sheet.write(i+row, columna+k, round(total, 2), mony_format)
+                # Total Impuestos
+                amount_total_tax = i_id.amount_total_company_signed - i_id.amount_untaxed_signed
+                sheet.write(i+row, columna+len(tax_ids)+0, round(amount_total_tax, 2), mony_format)
+                # Total
+                amount_total = i_id.amount_total_company_signed
+                sheet.write(i+row, columna+len(tax_ids)+1, round(amount_total , 2), mony_format)
+                # Moneda
+                sheet.write(i+row, columna+len(tax_ids)+2, i_id.currency_id.name, cell_format)
+                # Tipo de Cambio
+                sheet.write(i+row, columna+len(tax_ids)+3, exchange_rate, mony_format)
+                # Total en la Moneda de la Factura
+                invoice_currency_format = mony_format if i_id.currency_id == company_currency_id else workbook.add_format({'num_format': '%s #,##0.00' % i_id.currency_id.symbol})
+                sheet.write_formula(i+row, columna+len(tax_ids)+4, '=%s%s/%s%s' % (h[columna+len(tax_ids)+1], i+row+1, h[columna+len(tax_ids)+3], i+row+1), invoice_currency_format)
 
         # Report footer
-        f_index = fila+len(facturas)
+        f_index = row+len(facturas)
         sheet.write(f_index, 4, 'Totales', bold)
         # Subtotal
-        sheet.write_formula('%s%s'% (h[columna-1], f_index+1), '=SUM(%s%s:%s%s)' % (h[columna-1], fila, h[columna-1], f_index), money)
+        sheet.write_formula('%s%s'% (h[columna-1], f_index+1), '=SUM(%s%s:%s%s)' % (h[columna-1], row, h[columna-1], f_index), money)
         # Taxes
-        for i, tax in enumerate(taxes):
-            sheet.write_formula('%s%s'% (h[i+columna], f_index+1), '=SUM(%s%s:%s%s)' % (h[i+columna], fila+1, h[i+columna], f_index), money)
+        for i, tax in enumerate(tax_ids):
+            sheet.write_formula('%s%s'% (h[i+columna], f_index+1), '=SUM(%s%s:%s%s)' % (h[i+columna], row+1, h[i+columna], f_index), money)
         # Total Impuestos
-        h_index = columna+len(taxes)
-        sheet.write_formula('%s%s'% (h[h_index], f_index+1), '=SUM(%s%s:%s%s)' % (h[h_index], fila, h[h_index], f_index), money)
+        h_index = columna+len(tax_ids)
+        sheet.write_formula('%s%s'% (h[h_index], f_index+1), '=SUM(%s%s:%s%s)' % (h[h_index], row, h[h_index], f_index), money)
         # Total
-        sheet.write_formula('%s%s'% (h[h_index+1], f_index+1), '=SUM(%s%s:%s%s)' % (h[h_index+1], fila, h[h_index+1], f_index), money)
+        sheet.write_formula('%s%s'% (h[h_index+1], f_index+1), '=SUM(%s%s:%s%s)' % (h[h_index+1], row, h[h_index+1], f_index), money)
         
 
         # Ancho de columnas
@@ -192,17 +200,21 @@ class Facturas(models.AbstractModel):
         sheet.set_column('A:A', 15)
         # Nombre
         sheet.set_column('B:B', 30)
+        # Documento
+        sheet.set_column('C:C', 15)
         # Clave
-        sheet.set_column('C:C', 20)
-        # Consecutivo
         sheet.set_column('D:D', 20)
+        # Consecutivo
+        sheet.set_column('E:E', 20)
         # Fecha
-        sheet.set_column('E:E', 10)
+        sheet.set_column('F:F', 10)
         # Subtotal
-        sheet.set_column('F:F', 15)
+        sheet.set_column('G:G', 20)
         # Impuestos
-        sheet.set_column('%s:%s' % (h[columna], h[columna+len(taxes)+1]), 15)
+        sheet.set_column('%s:%s' % (h[columna], h[columna+len(tax_ids)+1]), 15)
         # Moneda
-        sheet.set_column('%s:%s' % (h[columna+len(taxes)+2], h[columna+len(taxes)+2]), 7)
+        sheet.set_column('%s:%s' % (h[columna+len(tax_ids)+2], h[columna+len(tax_ids)+2]), 7)
         # Tipo de Cambio
-        sheet.set_column('%s:%s' % (h[columna+len(taxes)+3], h[columna+len(taxes)+3]), 13)
+        sheet.set_column('%s:%s' % (h[columna+len(tax_ids)+3], h[columna+len(tax_ids)+3]), 13)
+        # Total en la Moneda de la Factura
+        sheet.set_column('%s:%s' % (h[columna+len(tax_ids)+4], h[columna+len(tax_ids)+4]), 20)
